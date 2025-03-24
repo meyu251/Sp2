ddec42dda2eee7e19bcb740c6b88f14e031498ae
Meyu251 <meyu251@gmail.com>
2025-03-24 11:41:39 +0200

    few more funcs for Graph

--- Ex1/Graph.cpp
+++ Ex1/Graph.cpp
@@ -4,12 +4,80 @@
 namespace graph {
 
 Graph::Graph(int n){
-    numOfVertices = n;
-    adjList = new Pair<int, DynamicArray<Pair<int, int>>>[n];  // first = vertex, second = list of pairs (neighbor, weight)
+    adjList = new DynamicArray<Pair<int, int>>[n];  // first = neighbore, second = weight
+    int numOfVertices = n;
 }
 
-Graph::~Graph() {
+Graph::~Graph(){
     delete[] adjList;
+}
+
+void Graph::addEdge(int src, int dest, int weight){
+    adjList[src - 1].push_back({dest, weight});
+    adjList[dest - 1].push_back({src, weight});
+}
+
+/*
+Add an undirected edge between src and dest
+*/
+void Graph::addEdge(int src, int dest){
+    adjList[src - 1].push_back({dest, 1});
+    adjList[dest - 1].push_back({src, 1});
+}
+
+bool Graph::hasEdge(int src, int dest){
+    for(int i = 0; i < adjList[src - 1].getSize(); i++){
+        if(adjList[src - 1].get(i).first == dest){
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+Remove an undirected edge between src and dest
+*/
+void Graph::removeEdge(int src, int dest){
+    if(!hasEdge(src, dest)){
+        throw std::invalid_argument("Edge does not exist.");
+        return;
+    }
+
+    for(int i = 0; i < adjList[src - 1].getSize(); i++){
+        if(adjList[src - 1].get(i).first == dest){
+            adjList[src - 1].removeAt(i);
+            break;
+        }
+    }
+    for(int i = 0; i < adjList[dest - 1].getSize(); i++){
+        if(adjList[dest - 1].get(i).first == src){
+            adjList[dest - 1].removeAt(i);
+            break;
+        }
+    }
+}
+
+/*
+Print the graph in the following format:
+{vertex1, neighbor (weight)}, {vertex1, neighbor (weight)}, ...
+{vertex2, neighbor (weight)}, {vertex2, neighbor (weight)}, ...
+.
+.
+.
+{vertexN, neighbor (weight)}, {vertexN, neighbor (weight)}, ..
+*/
+void Graph::printGraph(){
+    for(int i = 0; i < numOfVertices; i++){
+        for(int j = 0; j < adjList[i].getSize(); j++){
+            std::cout << "{" << i+1 << "," << adjList[i].get(j).first << " (" << adjList[i].get(j).second << ")}";
+            if(j != adjList[i].getSize() - 1){
+                std::cout << ", ";
+            }
+            else{
+                std::cout << std::endl;
+            }
+        }
+    }
 }
 
 }  // namespace graph
--- Ex1/Graph.hpp
+++ Ex1/Graph.hpp
@@ -7,13 +7,22 @@ namespace graph {
 
     class Graph {
     private:
+        // becouse the vertex numbered from 1 to n
+        // the array at index i represents the adjacency list of vertex (i+1)
+        DynamicArray<Pair<int, int>>* adjList;  // first = vertex, second = list of pairs (neighbor, weight)
         int numOfVertices;
-        Pair<int, DynamicArray<Pair<int, int>>>* adjList;  // first = vertex, second = list of pairs (neighbor, weight)
 
     public:
         
         Graph(int n);
         ~Graph();
+
+        void addEdge(int src, int dest, int weight);
+        void addEdge(int src, int dest);
+        bool hasEdge(int src, int dest);
+        void removeEdge(int src, int dest);
+        void printGraph();
+        
 
     };
 
